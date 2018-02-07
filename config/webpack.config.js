36a7091f9f6f9476718c4dcad14b832010e66289
krystof.matejka <krystof.matejka@economia.cz>
2018-02-07 17:31:58 +0100

    Refactor: Add test dependencies and edit config (refs #SKLEP-32)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,14 +3,13 @@ const path = require('path')
 module.exports = {
   context: __dirname,
   entry: path.resolve(__dirname, '../src/index.js'),
-  target: 'node',
+  target: 'web',
   output: {
-    path: path.resolve(__dirname, '../lib'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'feed-to-json.js',
     publicPath: '/',
     library: 'feed-to-json',
     libraryTarget: 'umd',
-
   },
   module: {
     loaders: [
